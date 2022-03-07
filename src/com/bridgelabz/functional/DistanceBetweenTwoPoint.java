698f46e234cd1cf2c0a7917c4bc8767f09b5bd22
Chougulenik <nikitachougule47@gmail.com>
2022-03-06 22:55:04 -0500

    Day6-FunctionalProgram added

---
+++ src/com/bridgelabz/functional/DistanceBetweenTwoPoint.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.functional;
+
+import java.util.Scanner;
+
+public class DistanceBetweenTwoPoint {
+
+	public static void main(String args[]) {
+		 
+		System.out.println("Enter the two point X & Y ");
+		Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        
+
+        // compute distance to (0, 0)
+        double dist = Math.sqrt(x*x + y*y);
+ 
+        // output distance
+        System.out.println("distance from (" + x + ", " + y + ") to (0, 0) = " + dist);
+
+	}
+
+}
