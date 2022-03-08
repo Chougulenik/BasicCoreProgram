3d3067cc8983fce04c4a6ed9b1ca3e9a6f897c64
Chougulenik <nikitachougule47@gmail.com>
2022-03-08 14:16:21 -0500

    added

--- src/com/bridgelabz/functional/CountTriplet.java
+++ src/com/bridgelabz/functional/CountTriplet.java
@@ -1,10 +1,36 @@
 package com.bridgelabz.functional;
 
+import java.util.Scanner;
+
 public class CountTriplet {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int n;
+        int[] array = new int[5];
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.println("Input number");
+        int num = input.nextInt();
+        sumTripplets(array,num);
+    }
+    public static void sumTripplets(int[] array, int num) {
+        System.out.println("Sum of Triplets are ");
+        for (int i = 0; i < array.length-2; i++)
+        {
+            for (int j = i+1; j < array.length-1; j++)
+            {
+                for (int k = j+1; k < array.length; k++)
+                {
+                    if (array[i]+array[j]+array[k] == num)
+                    {
+                        System.out.println("["+array[i]+", "+array[j]+", "+array[k]+"]");
+                        break;
+                    }
+                }
+            }
+       }       
 	}
 
 }
